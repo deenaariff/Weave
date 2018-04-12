18db0a6d9662fa92281dd503dc9a1369dd7130fa
daariff <aariff.deen@gmail.com>
2018-04-12 13:38:48 -0700

    Deleted redundant copies that were moved outside

--- src/main/java/raft/Raft.java
+++ src/main/java/raft/Raft.java
@@ -4,8 +4,8 @@ import node.RaftNode;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+/*import java.nio.file.Files;
+import java.nio.file.Paths;*/
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
@@ -24,7 +24,7 @@ public class Raft {
 
 	    Configuration config;
 		
-		// Create a new Raft Node
+		/* Create a new Raft Node
         Yaml yaml = new Yaml();
         try( InputStream in = Files.newInputStream( Paths.get( args[ 0 ] ) ) ) {
             config = yaml.loadAs( in, Configuration.class );
@@ -32,9 +32,9 @@ public class Raft {
             throw new RuntimeException("Unable to Locate Configuration File");
         }
 
-        Map<String,Integer> ports = config.getPorts();
+        Map<String,Integer> ports = config.getPorts(); */
 
-		RaftNode node = new RaftNode(ports.get("heartbeat"),ports.get("voting"));
+		RaftNode node = new RaftNode(8080,8081);
 		
 		System.out.println("Starting Raft Consensus Algorithm");
 
